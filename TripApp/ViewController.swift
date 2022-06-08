bf6cfe6d2f924386baea26198b408fb8ed2a5ab8
TYomiCH <timankov511@gmail.com>
2022-06-08 13:42:44 +0300

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by Artem on 8.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
