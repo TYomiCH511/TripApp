24bf2653e1732d791bd4316d2187ad0c9404ff3b
TYomiCH <timankov511@gmail.com>
2022-06-09 14:13:34 +0300

    Add Settin ViewController

---
+++ TripApp/BarController/TaBarViewController/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  TripApp
+//
+//  Created by Artem on 9.06.22.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
