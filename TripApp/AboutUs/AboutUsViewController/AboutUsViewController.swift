9570bb6aa2b99134ba61907eb390ca6e53bd23f2
TYomiCH <timankov511@gmail.com>
2022-06-17 12:47:27 +0300

    010 - add About us VC

---
+++ TripApp/AboutUs/AboutUsViewController/AboutUsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutUsViewController.swift
+//  TripApp
+//
+//  Created by Artem on 17.06.22.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
